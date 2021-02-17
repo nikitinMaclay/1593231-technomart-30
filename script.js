5f995f0de6cd0029db32e054e9eeea7260550c1e
nikitinMaclay <guicokiuytr@gmail.com>
2021-02-17 17:51:14 +0600

    добавление модальных окон

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
+let popupContact = document.querySelector('.modal-write-us');
+let openContactPopupButton = document.querySelector('.contacts-button');
+let closeContactPopupButton = popupContact.querySelector('.modal-closing-button');
 let popup = document.querySelector('.modal-map');
 let openPopupButton = document.querySelector('.contacts-img');
-let closePopupButton = popup.querySelector('.map-closing-button');
-let popupContent = document.querySelector('.modal-write-us');
-let openContactPopupButton = document.querySelector('.contacts-button');
-let closeContactPopupButton = popupContent.querySelector('.contact-closing-button');
+let closePopupButton = popup.querySelector('.modal-closing-button');
 
 openContactPopupButton.addEventListener('click', function (evt) {
   evt.preventDefault();
-  popupContent.classList.add('modal--show');
+  popupContact.classList.add('modal--show');
+});
+
+closeContactPopupButton.addEventListener('click', function () {
+  popupContact.classList.remove('modal--show');
 });
 
 openPopupButton.addEventListener('click', function (evt) {
@@ -15,14 +19,16 @@ openPopupButton.addEventListener('click', function (evt) {
   popup.classList.add('modal--show');
 });
 
-closeContactPopupButton.addEventListener('click', function () {
-  popupContent.classList.remove('modal--show');
-  console.log('убрали класс')
-});
-
-
 closePopupButton.addEventListener('click', function () {
   popup.classList.remove('modal--show');
 });
 
 
+// openCatalogPopupButton.addEventListener('click', function (evt) {
+//   evt.preventDefault();
+//   popupCatalog.classList.add('modal--show');
+// });
+
+// closeCatalogPopupButton.addEventListener('click', function () {
+//   popupCatalog.classList.remove('modal--show');
+// });
